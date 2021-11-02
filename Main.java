60d497e26205e5944bfc850b80c654ea87da858a
NeishaRindiani <92242673+NeishaRindiani@users.noreply.github.com>
2021-11-02 19:11:35 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratikum5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Neisha
+ */
+public class Main {
+     public static void main(String[] args) {
+        System.out.println("Program Start");
+        System.out.println("Inputan : ");
+        Scanner in = new Scanner(System.in);
+        Dadu dadu = new Dadu();
+        String kode;
+
+        while (true) {
+            kode = in.nextLine().toLowerCase();
+            if (kode.equals("q"))
+                break;
+            System.out.println(dadu.parse(kode));
+        }
+    }
+}
+
